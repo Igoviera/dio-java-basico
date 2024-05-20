6ca9df089cd43be7d1c4243b664a2a2a8c5e06d7
Igo <igo.vieira@live.com>
2024-05-20 12:23:15 -0300

    desafio 5 e 6 resolvido

--- desafio-Stream-API/src/Desafio04.java
+++ desafio-Stream-API/src/Desafio04.java
@@ -1,11 +1,19 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Desafio04 {
     public static void main(String[] args) {
-        //Desafio 3 - Verifique se todos os números da lista são positivos:
+        // Desafio 4 - Remova todos os valores ímpares:
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        System.out.println(numeros.stream().allMatch(n -> n > 0)); ;
+       List<Integer> numerosPares = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+
+       for (int n : numerosPares){
+           System.out.println(n);
+       }
     }
 }
